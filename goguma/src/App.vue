ff61c4f316e5b83db557da5c8c92b98d4358dcb8
CRYSTALRIM-PC\win10 <remember2889@gmail.com>
2018-04-29 19:22:44 +0900

    뷰시작,nav bar

---
+++ goguma/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <nav-tab></nav-tab>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Nav from './components/Nav'
+export default {
+  name: 'App',
+  components: {
+    "navTab" :Nav
+  }
+}
+</script>
+
+<style>
+
+*{
+  font-family: 'Noto Sans', sans-serif;
+}
+
+*{
+  margin: 0;
+  padding: 0;
+}
+</style>
